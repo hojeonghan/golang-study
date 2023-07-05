a92c5bc4ee0f910ad86b89cb1ad4d821db33c6cd
autumn.han <autumn.han@kakaoenterprise.com>
2023-07-05 18:10:47 +0900

    update

--- readfile/floatfile.go
+++ readfile/floatfile.go
@@ -34,3 +34,29 @@ func ReadFloatFile(filename string) ([3]float64, error) {
 
 	return numbers, nil
 }
+
+func SliceWithFile(filename string) ([]float64, error) {
+	var numbers []float64
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		numbers = append(numbers, number)
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+
+	return numbers, nil
+}
